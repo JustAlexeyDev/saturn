a6816b6ed2e05e0c33e9e5c527721f36a58af9a5
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2023-04-21 00:06:18 +0900

    Add files via upload

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,16 @@
+import saturn from '../assets/imgs/giphy.gif'
 export default function Home() {
   return (
-    <div>
+    <div class='home'>
       <div>
-      <h1>H O M E</h1>
+        {/* About US */}
+        <div class='about_main'>
+          <h1>Why "Saturn SkyLine"?</h1>
+          <p class='about_info'>What makes Saturn Saturn? His extraordinarily beautiful rings. Our company acts as the planet itself, and our customers are the very asteroids that form the rings.</p>
+          <span class='saturn_con'>
+            <img class='saturn' width={200} src={saturn} alt='saturn' />
+          </span>
+        </div>
       </div>
     </div>
   );
